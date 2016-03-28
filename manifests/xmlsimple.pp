4e846056f42b0ce09f452c0c1cd138e0c3bf375d
mh <mh@immerda.ch>
2016-03-28 11:39:04 +0200

    this is now packaged

--- manifests/xmlsimple.pp
+++ manifests/xmlsimple.pp
@@ -3,18 +3,9 @@ class rubygems::xmlsimple {
   package{'rubygem-xml-simple':
     ensure => present,
   }
-  case $::operatingsystem {
-    debian,ubuntu: {
-      Package['rubygem-xml-simple']{
-        name => 'libxml-simple-ruby'
-      }
-    }
-  }
-  if $::operatingsystem == 'CentOS' and $::operatingsystemmajrelease > 6 {
-    # not yet packaged
+  if $::osfamily == 'Debian' {
     Package['rubygem-xml-simple']{
-      name     => 'xml-simple',
-      provider => gem,
+      name => 'libxml-simple-ruby'
     }
   }
 }
